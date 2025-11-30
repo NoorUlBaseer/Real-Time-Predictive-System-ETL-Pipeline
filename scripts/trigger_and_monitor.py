ad707630e48c414bd5d8a65c1739acd7b6d53f30
Malaika1022 <malaika1022@gmail.com>
2025-11-30 11:02:43 +0500

    changed 1 line in trigger&monitor file

--- scripts/trigger_and_monitor.py
+++ scripts/trigger_and_monitor.py
@@ -43,7 +43,7 @@ def trigger_dag_with_retry(max_retries=60, delay=10):
             # Trigger the DAG
             response = requests.post(endpoint, headers=HEADERS, json=payload, allow_redirects=False)
 
-            if response.status_code == 201:
+            if response.status_code in [200, 201]:
                 run_id = response.json()["dag_run_id"]
                 print(f"✅ Success! Run ID: {run_id}")
                 return run_id
